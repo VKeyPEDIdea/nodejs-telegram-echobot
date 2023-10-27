475a0736afda8ab8d250f79b9b7dd92d1394e075
Pavel Karyapkin <pavel_karyapkin@epam.com>
2023-10-27 11:51:08 +0600

    add random number sample

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { Telegraf, Markup } = require('telegraf');
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const bot = new Telegraf(BOT_TOKEN);
 
-bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI echo messages. Please try it', {
-    })
-})
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// copy every message and send to the user
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
+const getCoinSide = () => getRandomInt(0, 1) === 0 ? 'Орёл' : 'Решка';
+const coinInlineKeyboard = Markup.inlineKeyboard([
+  Markup.button.callback('Подбросить ещё раз', 'flip_a_coin'),
+]);
 
-// Start webhook via launch method (preferred)
-bot.launch({
-    webhook: {
-      domain: process.env.URL,
-      port: process.env.PORT
-    }
-  })
-  
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+bot.hears('Подбросить монетку', ctx => ctx.reply(getCoinSide(), coinInlineKeyboard));
+bot.action('flip_a_coin', async(ctx) => {
+  await ctx.editMessageText(`${getCoinSide()}\nОтредактировано: ${new Date().toISOString()}`, coinInlineKeyboard);
+});
+
+const getRandomNumber = () => getRandomInt(0, 100);
+const numberInlineKeyboard = Markup.inlineKeyboard([
+  Markup.button.callback('Сгенерировать новое', 'random_number'),
+]);
+
+bot.hears('Случайное число', ctx => ctx.reply(getRandomNumber().toString(), numberInlineKeyboard));
+bot.action('random_number', async(ctx) => {
+  await ctx.editMessageText(`${getRandomNumber()}\nОтредактировано: ${new Date().toISOString()}`, numberInlineKeyboard);
+});
+
+bot.use(async (ctx) => {
+  await ctx.reply('Что нужно сделать?', Markup
+      .keyboard([
+          ['Подбросить монетку', 'Случайное число'],
+      ]).resize()
+  )
+});
+
+bot.launch().then(() => console.log('Started'));
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
